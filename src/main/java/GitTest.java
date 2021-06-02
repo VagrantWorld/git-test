b6bc2d7b6269376b692c3f46ed4a2746be2c26b2
yifeng <1114286524@qq.com>
2021-06-02 16:43:35 +0800

    third commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -2,5 +2,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("GIT TEST");
         System.out.println("GIT TEST2");
+        System.out.println("GIT TEST3");
     }
 }
